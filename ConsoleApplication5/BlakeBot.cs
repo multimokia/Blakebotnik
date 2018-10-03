4a72d01e33bc3d5a6a14ff24443c5e2ac3a55984
multimokia <multimokia3@gmail.com>
2018-10-03 19:37:00 -0400

    Adding Test!

--- ConsoleApplication5/BlakeBot.cs
+++ ConsoleApplication5/BlakeBot.cs
@@ -95,6 +95,10 @@ namespace ConsoleApplication5
             {
                 e.Channel.SendMessage(e.User.Mention + " NumNumNumNum touch my body\nC'mawn and touch my body!\nNumNumNumNum touch my body\nC'mawn and touch my body!\nI can see by the way that you're looking at me\nthat you wanna have a tiny little touch of my body\nSo go ahead, have a touch, cos it feels so nice\n'got the texture of carrots... and it tastes like rice\nI make it jiggle in the sun, make it jiggle in the rain,\njiggle in your eyes and jiggle in your brain,\njiggle on the floor and jiggle in the mirror,\njiggle on the ceiling....\nFricka fricka fricka fresh little jiggle, rides like a pony\nDoes anybody know if his real name's Tony?\nBarbra Streisand alone on a dock\nIf you don't like it you can suck my pocket watch\n\nNumNumNumNum touch my body\nC'mawn and touch my body!\nNumNumNumNum touch my body\nC'mawn and touch my body!\nx 4\n\nOh I once had a wiggle and his name was Greg,\nand every time that he would jiggle he would jump on his pegleg.\nI got a half shilling and a hand full of doodie\nand I'd really really like it if you'd shake ya bootie\nEvery body dance...and remove your underpants\nOh my Gosh!It's the attack of the killer ants!\nOh I once had a date with a unicorn\nWe went back to mine and we looked at porn!\nAlbert Fletcher alone on a raft\nwith a hippo, a lion and half a giraffe\nOne plays the strings, one plays the brass\nand the other one sits there smoking some grass.\nMeanwhile up in space a long time ago\nwas a man and a chicken and a deep fried afro,\nand a nun and a jeep and a packet of crisps\nand the empty wrapper of a Cadbury's Wispa\n\nNumNumNumNum touch my body\nC'mawn and touch my body!\nNumNumNumNum touch my body\nC'mawn and touch my body!\nx 4");
             }
+            if (e.Message.Text.ToLower.Equals("!test"))
+            {
+                e.Channel.SendMessage("Testing push!");
+            }
         }
     }
 }
